5232a7f6116cdb08c687553d893b17cc6d3d0ed5
Ben Pedrick <bpedrick@gmail.com>
2022-02-11 16:11:44 -0600

    PUT / replace value is now working

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const pool = require('../modules/pool');
 
+// =========== GET ALL ROWS FROM DB ====================
+
 router.get('/', (req, res) => {
     let queryText = 'SELECT * FROM "tasks";';
     pool.query(queryText).then(result => {
@@ -14,5 +16,39 @@ router.get('/', (req, res) => {
       res.sendStatus(500);
     });
   });
+
+// ======= ADD NEW TASK TO DB======================
+
+router.post('/', (req,res)=> {
+  let queryText = `INSERT INTO "tasks" ("name","done")
+  VALUES ($1, $2);`;
+  pool.query(queryText,[req.body.name,req.body.done])
+    .then(result => {
+      res.sendStatus(200);
+      console.log('task inserted successfully');
+    }).catch( err => {
+      console.log('Error on insert', err);
+    })
+})
+
+// ============ TOGGLE TASK DONE/UNDONE IN DB ============
+
+router.put('/:id', (req,res)=>{
+  let reqId = req.params.id;
+    console.log('PUT ID', reqId, req.body.done);
+    let queryText = `UPDATE "tasks"
+    SET "done" = $2
+    WHERE "id" = $1;  
+    `;
+    // req.body.done is goign to come in as ! to what it currently is. 
+    pool.query(queryText,[reqId,req.body.done])    
+    .then((result) => {
+      console.log('Task is flipped');
+      res.sendStatus(200);
+      }).catch((err)=>{
+        res.sendStatus(500);
+      })
+})
+
 
 module.exports = router;
